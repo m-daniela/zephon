42416f5e833411a9c9cd8e25d7e738f416250cac
m-daniela <54980615+m-daniela@users.noreply.github.com>
2023-09-07 09:37:41 +0300

    BE add conversation refactoring, delete conversation skeleton

---
+++ server/database/delete_data.ts
@@ -0,0 +1,12 @@
+
+
+export const deleteConversation = async (conversationId: string) => {
+    /**
+     * find conversation
+     * find participants
+     * delete conversation from participants' convo list
+     * delete conversation
+     * * delete messages from collection
+     */
+    // const conversationRef = 
+};
