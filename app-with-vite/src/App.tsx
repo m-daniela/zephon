39d8b19da12e9aee7c57904c2a5542ecc23ec257
m-daniela <54980615+m-daniela@users.noreply.github.com>
2023-10-18 12:01:49 +0300

    FE added routing scaffolding

--- app-with-vite/src/App.tsx
+++ app-with-vite/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     return (
         <section>
           Vite app home
+            <Link to={"login"}>Go to login</Link>
         </section>
     );
 }
